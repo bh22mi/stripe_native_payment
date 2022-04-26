fe10db1cc2646ada532f858c68103782f69496dc
Bhoomika Harkhani <bh22mi@gmail.com>
2022-04-26 18:13:19 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,14 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:stripe_payment_reusable/main.dart';
+import 'package:stripe_payment_reusable/main.dart';
 
-// void main() {
-//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//     // Build our app and trigger a frame.
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
 //     await tester.pumpWidget(MyApp());
 
 //     // Verify that our counter starts at 0.
@@ -26,5 +26,5 @@
 //     // Verify that our counter has incremented.
 //     expect(find.text('0'), findsNothing);
 //     expect(find.text('1'), findsOneWidget);
-//   });
-// }
+  });
+}
